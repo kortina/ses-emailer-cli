5d24e3249235c2927c72b1765b32748e0488b0b1
Kortina <kortina@gmail.com>
2020-08-21 08:10:52 -0700

    also send note to self in body

--- note-to-self.sh
+++ note-to-self.sh
@@ -7,9 +7,15 @@
 #
 # I define all of these in ~/.ses_conf_private and source that from my .bash_profile
 
-SUBJECT=$(cat) # read from STDIN
+RAW=$(cat) # read from STDIN
 touch $NOTE_TO_SELF_LOG
-echo -e "$SUBJECT" >> $NOTE_TO_SELF_LOG
+echo -e "$RAW" >> $NOTE_TO_SELF_LOG
+BODY="$RAW"
+SUBJECT=$(echo "$RAW" | tr '\n' ' ' | tr '\r' ' ' | head -c 80) # truncate to 80 chars
 cd "$( dirname "${BASH_SOURCE[0]}" )" # change to cwd
-python ses.py send_email --sender="$NOTE_TO_SELF_FROM"  --to="$NOTE_TO_SELF_TO" --subject="$SUBJECT" --body=" "
-echo -e "To: $NOTE_TO_SELF_TO\n$SUBJECT"
+python ses.py send_email \
+    --sender="$NOTE_TO_SELF_FROM"  \
+    --to="$NOTE_TO_SELF_TO" \
+    --subject="$SUBJECT" \
+    --body="$BODY"
+echo -e "To: $NOTE_TO_SELF_TO\n$BODY"
